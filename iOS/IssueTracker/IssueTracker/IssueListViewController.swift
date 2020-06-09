ecdfba110980fc373dab938da65647b422cb06e1
kjoonk <kjoonk@gmail.com>
2020-06-09 15:55:43 +0900

    [iOS] feat: IssueList 뷰 구성
    issue #5

--- iOS/IssueTracker/IssueTracker/IssueListViewController.swift
+++ iOS/IssueTracker/IssueTracker/IssueListViewController.swift
@@ -1,17 +1,9 @@
 import UIKit
 
 class IssueListViewController: UIViewController {
-    @IBOutlet weak var titleLabel: UILabel!
-    @IBOutlet weak var bodyView: UITextView!
-    @IBOutlet weak var ownerLabel: UILabel!
-
-    lazy var issue: Issue = .init(id: 1, title: "첫 번째 이슈", body: "이슈 한 바퀴 \n이슈 목록 만들기", owner: User(id: 1, name: "Jane Doe"))
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        titleLabel.text = issue.title
-        bodyView.text = issue.body
-        ownerLabel.text = String(describing: issue.owner)
     }
+
 }
--- iOS/IssueTracker/IssueTrackerTests/IssueTrackerTests.swift
+++ iOS/IssueTracker/IssueTrackerTests/IssueTrackerTests.swift
@@ -3,7 +3,7 @@ import XCTest
 
 class IssueTrackerTests: XCTestCase {
     func testViewController() {
-        let viewController = viewControllerDidLoadView(identifier: "VC") as! IssueListViewController
+        let viewController = viewControllerDidLoadView(identifier: "VC") as! IssueDetailViewController
 
         let issue: Issue = .init(id: 1, title: "첫 번째 이슈 테스트", body: "이슈 한 바퀴 \n이슈 목록 만들기", owner: User(id: 1, name: "Jane Doe"))
         viewController.issue = issue
