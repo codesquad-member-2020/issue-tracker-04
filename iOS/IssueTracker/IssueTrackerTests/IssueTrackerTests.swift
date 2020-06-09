ce9e0a4f6b84f9f5efe99b42d7a3c72793fc6450
dev-Lena <keunnalee@gmail.com>
2020-06-09 18:05:25 +0900

    [iOS] refactor: IssueList 데이터 소스 test 코드 작성
    Issue #5

--- iOS/IssueTracker/IssueTrackerTests/IssueTrackerTests.swift
+++ iOS/IssueTracker/IssueTrackerTests/IssueTrackerTests.swift
@@ -3,7 +3,7 @@ import XCTest
 
 class IssueTrackerTests: XCTestCase {
     func testViewController() {
-        let viewController = viewControllerDidLoadView(identifier: "VC") as! IssueDetailViewController
+        let viewController = viewControllerDidLoadView(identifier: "IssueDetailViewController") as! IssueDetailViewController
 
         let issue: Issue = .init(id: 1, title: "첫 번째 이슈 테스트", body: "이슈 한 바퀴 \n이슈 목록 만들기", owner: User(id: 1, name: "Jane Doe"))
         viewController.issue = issue
@@ -14,7 +14,30 @@ class IssueTrackerTests: XCTestCase {
         XCTAssertEqual(viewController.titleLabel.text, issue.title)
 
     }
-
+    
+    func testConfigureIssueListDataSource() {
+        
+        // given
+        let user = User(id: 1, name: "모오오오스")
+        let issueList: IssueCollection = [Issue(id: 1, title: "title1", body: nil, owner: user),Issue(id: 2, title: "title2", body: "Something", owner: user),Issue(id: 3, title: "title3", body: "Special", owner: user)]
+        let dataSource = IssueListDataSource(issueList)
+        let tableView = UITableView()
+        tableView.register(IssueCell.self, forCellReuseIdentifier: IssueCell.identifier)
+        
+        // when
+        let cellRow = 1
+        let numberOfRows = dataSource.tableView(tableView, numberOfRowsInSection: 0)
+        let secondCell = dataSource.tableView(tableView, cellForRowAt: IndexPath(row: cellRow, section: 0)) as! IssueCell
+        
+        // then
+        XCTAssertEqual(numberOfRows, issueList.count)
+        // FIXME: 오류 발생 (nil)
+        XCTAssertEqual(secondCell.titleLabel.text, issueList[cellRow].title)
+        XCTAssertEqual(secondCell.detailLabel.text, issueList[cellRow].body)
+    }
+}
+ 
+private extension IssueTrackerTests {
     func viewControllerDidLoadView(identifier: String) -> UIViewController {
         let storyboard = UIStoryboard(name: "Main", bundle: nil)
         let viewController = storyboard.instantiateViewController(identifier: identifier)
