b0bc1ec6ad64cecdfcf1394d3bb916fdcbc2aff2
kjoonk <kjoonk@gmail.com>
2020-06-11 00:56:23 +0900

    [iOS] feat: 이슈 작성 뷰 컨트롤러에 기본 동작 구현
    - unwindSegue 사용하여 save 처리
    - 상수 관리를 위해 Constant 추가
    
    issue #7

---
+++ iOS/IssueTracker/IssueTracker/Constants.swift
@@ -0,0 +1,10 @@
+enum SystemImageName {
+    static let cellShare = "checkmark.rectangle"
+    static let cellDelete = "trash"
+}
+
+enum Identifier {
+    enum Segue {
+        static let save = "Save"
+    }
+}
