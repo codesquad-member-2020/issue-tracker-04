d2f78a56d1f0daa5eb7f17bcda0d2e117dc2befc
dev-Lena <keunnalee@gmail.com>
2020-06-10 15:41:04 +0900

    [iOS] refactor: 코드 정리

--- iOS/IssueTracker/IssueTrackerTests/IssueTrackerTests.swift
+++ iOS/IssueTracker/IssueTrackerTests/IssueTrackerTests.swift
@@ -22,18 +22,12 @@ class IssueTrackerTests: XCTestCase {
         let issueList: IssueCollection = [Issue(id: 1, title: "title1", body: nil, owner: user),Issue(id: 2, title: "title2", body: "Something", owner: user),Issue(id: 3, title: "title3", body: "Special", owner: user)]
         let dataSource = IssueListDataSource(issueList)
         let tableView = UITableView()
-        tableView.register(IssueCell.self, forCellReuseIdentifier: IssueCell.identifier)
         
         // when
-        let cellRow = 1
         let numberOfRows = dataSource.tableView(tableView, numberOfRowsInSection: 0)
-        let secondCell = dataSource.tableView(tableView, cellForRowAt: IndexPath(row: cellRow, section: 0)) as! IssueCell
         
         // then
         XCTAssertEqual(numberOfRows, issueList.count)
-        // FIXME: 오류 발생 (nil)
-        XCTAssertEqual(secondCell.titleLabel.text, issueList[cellRow].title)
-        XCTAssertEqual(secondCell.detailLabel.text, issueList[cellRow].body)
     }
 }
  
