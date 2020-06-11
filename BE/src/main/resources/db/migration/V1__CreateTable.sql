8333d558da9fc42abbff802ca0435896b4a4df1c
Jin Hyung Park <hophfg@yahoo.co.kr>
2020-06-11 14:49:48 +0900

    feat: update and drop table

---
+++ BE/src/main/resources/db/migration/V1__CreateTable.sql
@@ -0,0 +1,92 @@
+DROP TABLE IF EXISTS label_has_issue;
+DROP TABLE IF EXISTS assignee;
+DROP TABLE IF EXISTS emoji;
+DROP TABLE IF EXISTS photo;
+DROP TABLE IF EXISTS label;
+DROP TABLE IF EXISTS comment;
+DROP TABLE IF EXISTS issue;
+DROP TABLE IF EXISTS milestone;
+DROP TABLE IF EXISTS user;
+
+CREATE TABLE IF NOT EXISTS user
+(
+    id        INT          NOT NULL,
+    name      VARCHAR(45)  NULL,
+    github_id VARCHAR(45)  NULL,
+    image     VARCHAR(500) NULL,
+    PRIMARY KEY (id)
+);
+
+CREATE TABLE IF NOT EXISTS milestone
+(
+    `id`          INT         NOT NULL,
+    `title`       VARCHAR(45) NULL,
+    `duedate`     DATETIME    NULL,
+    `description` VARCHAR(45) NULL,
+    PRIMARY KEY (`id`)
+);
+
+CREATE TABLE IF NOT EXISTS issue
+(
+    `id`           INT         NOT NULL,
+    `title`        VARCHAR(45) NULL,
+    `milestone_id` INT         NOT NULL,
+    PRIMARY KEY (`id`, `milestone_id`),
+    CONSTRAINT `fk_issue_milestone1` FOREIGN KEY (`milestone_id`) REFERENCES `milestone` (`id`)
+);
+
+CREATE TABLE IF NOT EXISTS comment
+(
+    `id`         INT      NOT NULL,
+    `content`    TEXT     NULL,
+    `created_at` DATETIME NULL,
+    `updated_at` DATETIME NULL,
+    `user_id`    INT      NOT NULL,
+    `issue_id`   INT      NOT NULL,
+    PRIMARY KEY (`id`, `user_id`),
+    CONSTRAINT `fk_comment_user1` FOREIGN KEY (`user_id`) REFERENCES `user` (`id`),
+    CONSTRAINT `fk_comment_issue1` FOREIGN KEY (`issue_id`) REFERENCES `issue` (`id`)
+);
+
+CREATE TABLE IF NOT EXISTS label
+(
+    `id`          INT          NOT NULL,
+    `name`        VARCHAR(45)  NULL,
+    `color`       VARCHAR(45)  NULL,
+    `description` VARCHAR(100) NULL,
+    PRIMARY KEY (`id`)
+);
+
+CREATE TABLE IF NOT EXISTS photo
+(
+    `url`        TEXT NULL,
+    `comment_id` INT  NOT NULL,
+    PRIMARY KEY (`comment_id`),
+    CONSTRAINT `fk_photo_comment1` FOREIGN KEY (`comment_id`) REFERENCES `comment` (`id`)
+);
+
+CREATE TABLE IF NOT EXISTS emoji
+(
+    `name`       VARCHAR(45) NULL,
+    `comment_id` INT         NOT NULL,
+    PRIMARY KEY (`comment_id`),
+    CONSTRAINT `fk_emoji_comment1` FOREIGN KEY (`comment_id`) REFERENCES `comment` (`id`)
+);
+
+CREATE TABLE IF NOT EXISTS assignee
+(
+    `issue_id` INT NOT NULL,
+    `user_id`  INT NOT NULL,
+    PRIMARY KEY (`issue_id`, `user_id`),
+    CONSTRAINT `fk_issue_has_user_issue` FOREIGN KEY (`issue_id`) REFERENCES `issue` (`id`),
+    CONSTRAINT `fk_issue_has_user_user1` FOREIGN KEY (`user_id`) REFERENCES `user` (`id`)
+);
+
+CREATE TABLE IF NOT EXISTS label_has_issue
+(
+    `label_id` INT NOT NULL,
+    `issue_id` INT NOT NULL,
+    PRIMARY KEY (`label_id`, `issue_id`),
+    CONSTRAINT `fk_label_has_issue_label1` FOREIGN KEY (`label_id`) REFERENCES `label` (`id`),
+    CONSTRAINT `fk_label_has_issue_issue1` FOREIGN KEY (`issue_id`) REFERENCES `issue` (`id`)
+);
