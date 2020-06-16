b909b90fa1be7d97b108ca6c177bf7cbaf687a3b
Jin Hyung Park <hophfg@yahoo.co.kr>
2020-06-16 15:39:47 +0900

    feat: add LabelService and LabelServiceTest

--- BE/src/test/java/com/codesquad/issue04/service/LabelServiceTest.java
+++ BE/src/test/java/com/codesquad/issue04/service/LabelServiceTest.java
@@ -1,4 +1,23 @@
 package com.codesquad.issue04.service;
 
+import static org.assertj.core.api.Assertions.*;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import com.codesquad.issue04.domain.label.Label;
+
+@SpringBootTest
 public class LabelServiceTest {
+
+	@Autowired
+	private LabelService labelService;
+
+	@DisplayName("라벨이 불러와진다.")
+	@Test
+	void 라벨이_불러와진다() {
+		assertThat(labelService.findLabelById(1L)).isInstanceOf(Label.class);
+	}
 }
