a73fe0af333715e3f7055ccfba959c04662f35fa
dev-Lena <keunnalee@gmail.com>
2020-06-19 13:11:11 +0900

    [iOS] feat: 이슈 정보 하단 메뉴 표시 구현
    - 상태 관리
    - 애니메이션
    - 배경 블러 효과
    Issue #62, #63

--- iOS/IssueTracker/IssueTracker/IssueDetail/IssueDetailViewController.swift
+++ iOS/IssueTracker/IssueTracker/IssueDetail/IssueDetailViewController.swift
@@ -1,11 +1,28 @@
 import UIKit
 
 class IssueDetailViewController: UIViewController {
+    
+    enum IssueInfoState {
+        case expanded
+        case collpased
+    }
+
     @IBOutlet weak var titleLabel: UILabel!
     @IBOutlet weak var bodyView: UITextView!
     @IBOutlet weak var ownerLabel: UILabel!
 
     var issue: Issue
+    var issueInfoViewController: IssueInfoViewController!
+    var handleAreaHeight:CGFloat {self.view.frame.height * 0.2}
+    var issueInfoVisible = false
+    var visualEffectView: UIVisualEffectView!
+    var issueInfoViewHeight:CGFloat  {
+        self.view.frame.height * 0.8
+    }
+    var nextState:IssueInfoState {
+        issueInfoVisible ? .collpased : .expanded
+    }
+
 
     init?(coder: NSCoder, issue: Issue) {
         self.issue = issue
@@ -22,7 +39,79 @@ class IssueDetailViewController: UIViewController {
         titleLabel.text = issue.title
         bodyView.text = issue.body
         ownerLabel.text = String(describing: issue.owner)
+        setupButtomIssueInfoView()
+
     }
+    
+    func setupButtomIssueInfoView() {
+        setupVisualEffectView()
+        setupIssueInfoView()
+        
+    }
+    private func setupVisualEffectView() {
+        visualEffectView = UIVisualEffectView(frame: self.view.bounds) // 블러 화면
+        self.view.addSubview(visualEffectView)
+        
+        visualEffectView.isHidden = true
+    }
+    
+    private func setupIssueInfoView() {
+        issueInfoViewController = IssueInfoViewController(nibName:"IssueInfoViewController",bundle:nil)
+        self.addChild(issueInfoViewController)
+        self.view.addSubview(issueInfoViewController.view)
+        issueInfoViewController.view.frame = CGRect(x:0,y:self.view.frame.height - handleAreaHeight, width:self.view.frame.width, height:issueInfoViewHeight)
+        issueInfoViewController.view.clipsToBounds = true
+        
+        issueInfoViewController.view.layer.cornerRadius = 15
+        
+        let panGestureRecognizer = UIPanGestureRecognizer(target: self, action: #selector(gestureStarted))
+        issueInfoViewController.handlerArea.addGestureRecognizer(panGestureRecognizer)
+        
+        issueInfoViewController.moveBeforeCommentButton.addTarget(self, action: #selector(moveToPreviousComment), for: .touchUpInside)
+        issueInfoViewController.moveNextCommentButton.addTarget(self, action: #selector(moveToNextComment), for: .touchUpInside)
+    }
+    
+    // MARK: - Selector
+    
+    @objc func gestureStarted(_ recognizer:UIPanGestureRecognizer) {
+        self.visualEffectView.isHidden = false
+        createAnimation(state: nextState, duration: 0.5)
+    }
+    
+    // MARK: - Animation
+    
+    private func createAnimation(state:IssueInfoState, duration:TimeInterval) {
+        let cardMoveUpAnimation = UIViewPropertyAnimator(duration: duration, curve: .linear) { [weak self] in
+            guard let `self` = self else  { return }
+            switch state {
+            case .collpased:
+                self.issueInfoViewController.view.frame.origin.y = self.view.frame.height - self.handleAreaHeight
+            case .expanded:
+                self.issueInfoViewController.view.frame.origin.y = self.view.frame.height - self.issueInfoViewHeight
+            }
+        }
+        cardMoveUpAnimation.addCompletion { [weak self] _ in
+            self?.issueInfoVisible =  state ==  .collpased ? false : true
+        }
+        cardMoveUpAnimation.startAnimation()
+        
+        createBlurViewAnimation(state: state, duration: duration)
+        
+    }
+    
+    private func createBlurViewAnimation(state:IssueInfoState, duration:TimeInterval) {
+        let visualEffectAnimation = UIViewPropertyAnimator.init(duration: duration, curve: .linear) { [weak self ] in
+            switch state {
+            case .expanded:
+                self?.visualEffectView.effect = UIBlurEffect(style: .dark)
+                
+            case .collpased:
+                self?.visualEffectView.effect =  nil
+            }
+        }
+        visualEffectAnimation.startAnimation()
+    }
+
 
     // MARK: - Navigation
 
