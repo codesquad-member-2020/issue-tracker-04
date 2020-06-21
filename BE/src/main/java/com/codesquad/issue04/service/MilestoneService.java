a69c81353a33d68721f9b88c8ad42dc3975bdfea
HYUNJUN <guswns1659@gmail.com>
2020-06-21 16:04:45 +0900

    [BE] feat : 필터 시 모든 마일스톤 응답하는 API 구현
    - MilestoneResponseDto -> MilestoneDto 이름 변경.
      - 응답되는 Dto가 아니기 때문.
    - MilestoneController.getAllMilestones() 생성.
    - MilestoneControllerTest.전체_마일스톤을_응답한다() 테스트 통과.

--- BE/src/main/java/com/codesquad/issue04/service/MilestoneService.java
+++ BE/src/main/java/com/codesquad/issue04/service/MilestoneService.java
@@ -7,8 +7,8 @@ import org.springframework.stereotype.Service;
 
 import com.codesquad.issue04.domain.milestone.Milestone;
 import com.codesquad.issue04.domain.milestone.MilestoneRepository;
-import com.codesquad.issue04.web.dto.response.label.MilestoneResponseDtos;
-import com.codesquad.issue04.web.dto.response.milestone.MilestoneResponseDto;
+import com.codesquad.issue04.web.dto.response.milestone.MilestoneResponseDtos;
+import com.codesquad.issue04.web.dto.response.milestone.MilestoneDto;
 import lombok.RequiredArgsConstructor;
 
 @Service
@@ -17,25 +17,25 @@ public class MilestoneService {
 
 	private final MilestoneRepository milestoneRepository;
 
-	protected Milestone findMilestoneById(Long labelId) {
-		return milestoneRepository.findById(labelId)
-			.orElseThrow(() -> new IllegalArgumentException("label not found id: " + labelId));
+	public MilestoneResponseDtos getMilestoneOverviews() {
+		return MilestoneResponseDtos.builder()
+			.allData(getAllMilestones())
+			.build();
+	}
+
+	protected List<MilestoneDto> getAllMilestones() {
+		List<Milestone> milestones = findAllMilestones();
+		return milestones.stream()
+			.map(MilestoneDto::of)
+			.collect(Collectors.toList());
 	}
 
 	protected List<Milestone> findAllMilestones() {
 		return milestoneRepository.findAll();
 	}
 
-	protected List<MilestoneResponseDto> getAllMilestones() {
-		List<Milestone> milestones = findAllMilestones();
-		return milestones.stream()
-			.map(MilestoneResponseDto::of)
-			.collect(Collectors.toList());
-	}
-
-	public MilestoneResponseDtos getMilestoneOverviews() {
-		return MilestoneResponseDtos.builder()
-			.allData(getAllMilestones())
-			.build();
+	protected Milestone findMilestoneById(Long labelId) {
+		return milestoneRepository.findById(labelId)
+			.orElseThrow(() -> new IllegalArgumentException("label not found id: " + labelId));
 	}
 }
--- BE/src/test/java/com/codesquad/issue04/web/controller/MilestoneControllerTest.java
+++ BE/src/test/java/com/codesquad/issue04/web/controller/MilestoneControllerTest.java
@@ -1,4 +1,42 @@
 package com.codesquad.issue04.web.controller;
 
+import static org.assertj.core.api.Assertions.*;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.reactive.AutoConfigureWebTestClient;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.web.server.LocalServerPort;
+import org.springframework.http.HttpStatus;
+import org.springframework.test.web.reactive.server.EntityExchangeResult;
+import org.springframework.test.web.reactive.server.WebTestClient;
+
+import com.codesquad.issue04.web.dto.response.milestone.MilestoneResponseDtos;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@AutoConfigureWebTestClient
 public class MilestoneControllerTest {
+
+    @LocalServerPort
+    private int port;
+
+    @Autowired
+    private WebTestClient webTestClient;
+
+    @DisplayName("전체 마일스톤을 응답하는 테스트")
+    @Test
+    void 전체_마일스톤을_응답한다() {
+        String url = "http://localhost:" + port + "/api/v1/allMilestones";
+
+        EntityExchangeResult<MilestoneResponseDtos> entityExchangeResult = webTestClient.get()
+            .uri(url)
+            .exchange()
+            .expectStatus().isEqualTo(HttpStatus.OK)
+            .expectBody(MilestoneResponseDtos.class)
+            .returnResult();
+
+        assertThat(entityExchangeResult.getResponseBody()).isInstanceOf(MilestoneResponseDtos.class);
+        assertThat(entityExchangeResult.getResponseBody().getAllData().get(0).getTitle()).isEqualTo("1차 배포");
+    }
 }
