8a084cf94ce673bb8f8288d86952e720e8edf91c
Jin Hyung Park <hophfg@yahoo.co.kr>
2020-06-22 12:38:13 +0900

    test: create label test

--- BE/src/test/java/com/codesquad/issue04/service/LabelServiceTest.java
+++ BE/src/test/java/com/codesquad/issue04/service/LabelServiceTest.java
@@ -1,15 +1,19 @@
 package com.codesquad.issue04.service;
 
 import static org.assertj.core.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.*;
 
 import java.util.List;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 import com.codesquad.issue04.domain.label.Label;
+import com.codesquad.issue04.web.dto.request.LabelCreateRequestDto;
 import com.codesquad.issue04.web.dto.response.label.LabelDetailResponseDto;
 import com.codesquad.issue04.web.dto.response.label.LabelOverviewDto;
 import com.codesquad.issue04.web.dto.response.label.LabelOverviewResponseDtos;
@@ -42,5 +46,19 @@ public class LabelServiceTest {
 	@Test
 	void 라벨_구체_정보가_불러와진다() {
 		assertThat(labelService.findLabelDetailById(1L)).isInstanceOf(LabelDetailResponseDto.class);
+	}
+
+	@DisplayName("라벨 하나가 추가된다.")
+	@CsvSource({"BE, #F2DUS3, 백엔드 작업이슈"})
+	@ParameterizedTest
+	void 라벨_하나가_추가된다(String title, String color, String description) {
+		LabelCreateRequestDto dto = new LabelCreateRequestDto(title, color, description);
+		labelService.createNewLabel(dto);
+		LabelDetailResponseDto savedLabelDto = labelService.findLatestIssue();
+		assertAll(
+			() -> assertThat(savedLabelDto.getTitle()).isEqualTo(title),
+			() -> assertThat(savedLabelDto.getColor()).isEqualTo(color),
+			() -> assertThat(savedLabelDto.getDescription()).isEqualTo(description)
+		);
 	}
 }
