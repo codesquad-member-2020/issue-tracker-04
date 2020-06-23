e59b20ec89e76ed54c8bfa45296ee9bd3375424c
Jin Hyung Park <hophfg@yahoo.co.kr>
2020-06-23 16:47:07 +0900

    feat: cleaning and migrating flyway when application starts

--- BE/src/main/java/com/codesquad/issue04/Issue04Application.java
+++ BE/src/main/java/com/codesquad/issue04/Issue04Application.java
@@ -1,13 +1,31 @@
 package com.codesquad.issue04;
 
+import org.flywaydb.core.Flyway;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.flyway.FlywayMigrationStrategy;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Profile;
 
 @SpringBootApplication
 public class Issue04Application {
 
 	public static void main(String[] args) {
 		SpringApplication.run(Issue04Application.class, args);
+	}
+
+	@Bean
+	@Profile("local")
+	public FlywayMigrationStrategy cleanMigrateStrategy() {
+		FlywayMigrationStrategy strategy = new FlywayMigrationStrategy() {
+			@Override
+			public void migrate(Flyway flyway) {
+				flyway.clean();
+				flyway.migrate();
+			}
+		};
+
+		return strategy;
 	}
 
 }
