fe2b305cb8e718118e451555f8ee32d38aa26c88
Jin Hyung Park <hophfg@yahoo.co.kr>
2020-06-24 18:18:05 +0900

    refactor: remove unnecessary method and change coding convention

--- BE/src/main/java/com/codesquad/issue04/domain/user/RealUser.java
+++ BE/src/main/java/com/codesquad/issue04/domain/user/RealUser.java
@@ -13,7 +13,6 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.JoinTable;
-import javax.persistence.ManyToMany;
 import javax.persistence.OneToMany;
 import javax.persistence.Table;
 
@@ -36,58 +35,49 @@ import lombok.ToString;
 @Table(name = "user")
 public class RealUser implements Serializable, AbstractUser {
 
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
-    private String name;
+	@JsonIgnore
+	@OneToMany(orphanRemoval = true, fetch = FetchType.LAZY)
+	@JoinTable(
+		name = "assignee",
+		joinColumns = @JoinColumn(name = "user_id"),
+		inverseJoinColumns = @JoinColumn(name = "issue_id")
+	)
+	protected List<Issue> assignedIssues;
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	private String name;
+	@Column(name = "github_id")
+	private String githubId;
+	private String image;
+	@JsonIgnore
+	@OneToMany(cascade = CascadeType.ALL, mappedBy = "user")
+	private List<Issue> ownedIssues;
 
-    @Column(name = "github_id")
-    private String githubId;
-    private String image;
+	public static RealUser of(GithubUser githubUser) {
+		return RealUser.builder()
+			.githubId(githubUser.getUserId())
+			.name(githubUser.getName())
+			.image(githubUser.getImage())
+			.assignedIssues(Collections.emptyList())
+			.ownedIssues(Collections.emptyList())
+			.build();
+	}
 
-    @JsonIgnore
-    @OneToMany(cascade = CascadeType.ALL, mappedBy = "user")
-    private List<Issue> ownedIssues;
+	@Override
+	public boolean isNil() {
+		return false;
+	}
 
-    @JsonIgnore
-    @ManyToMany(fetch = FetchType.LAZY)
-    @JoinTable(
-        name = "assignee",
-        joinColumns = @JoinColumn(name = "user_id"),
-        inverseJoinColumns = @JoinColumn(name = "issue_id")
-    )
-    protected List<Issue> assignedIssues;
+	public boolean hasOwnedIssues() {
+		return this.getOwnedIssues().size() > 0;
+	}
 
-    public static RealUser of(GithubUser githubUser) {
-        return RealUser.builder()
-            .githubId(githubUser.getUserId())
-            .name(githubUser.getName())
-            .image(githubUser.getImage())
-            .assignedIssues(Collections.emptyList())
-            .ownedIssues(Collections.emptyList())
-            .build();
-    }
+	public boolean hasAssignedIssues() {
+		return this.getAssignedIssues().size() > 0;
+	}
 
-    public RealUser update(final String name, final String picture) {
-        this.name = name;
-        this.image = picture;
-        return this;
-    }
-
-    @Override
-    public boolean isNil() {
-        return false;
-    }
-
-    public boolean hasOwnedIssues() {
-        return this.getOwnedIssues().size() > 0;
-    }
-
-    public boolean hasAssignedIssues() {
-        return this.getAssignedIssues().size() > 0;
-    }
-
-    public boolean isMatchedGitHubId(String userGithubId) {
-        return this.githubId.equals(userGithubId);
-    }
+	public boolean isMatchedGitHubId(String userGithubId) {
+		return this.githubId.equals(userGithubId);
+	}
 }
